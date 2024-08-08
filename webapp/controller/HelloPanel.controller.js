e6b8a3a1137bc9a14b1df103f5df3ce1591a2bf8
Avinash Vaidya <avinash.vaidya@sap.com>
2024-08-08 02:16:39 +0000

    Added Dialog Fragment

--- webapp/controller/HelloPanel.controller.js
+++ webapp/controller/HelloPanel.controller.js
@@ -2,9 +2,10 @@ sap.ui.define([
     "sap/ui/core/mvc/Controller",
     "sap/m/MessageToast",
     "sap/ui/model/json/JSONModel",
-    "sap/ui/model/resource/ResourceModel"
+    "sap/ui/model/resource/ResourceModel",
+    "sap/ui/core/Fragment"
 ],
-    function (Controller, MessageToast) {
+    function (Controller, MessageToast, Fragment) {
         "use strict";
 
         return Controller.extend("com.learn.ui5.sapui5basics.controller.HelloPanel", {
@@ -20,6 +21,23 @@ sap.ui.define([
 
                 // Show UI5 alert
                 MessageToast.show(finalMessage);
+            },
+            onOpenDialog: function () {
+                var oView = this.getView();
+
+                // Create dialog lazily
+                if (!this.byId("helloDialog")) {
+                    // Load the fragment definition and add it to the view
+                    // Load the fragment definition and add it to the view
+                    var oDialog = sap.ui.xmlfragment(oView.getId(), "com.learn.ui5.sapui5basics.view.fragment.HelloDialog", this);
+                    oView.addDependent(oDialog);
+                    oDialog.open();
+                } else {
+                  this.byId("helloDialog").open();
+                }
+            },
+            onCloseDialog: function () {
+                this.byId("helloDialog").close();
             }
         });
     });
