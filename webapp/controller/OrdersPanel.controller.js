e3d806015985b0b533d33e7833574f6824e7f5e5
Avinash Vaidya <avinash.vaidya@sap.com>
2024-08-09 13:59:21 +0000

    Created Orders panel to display list of orders from the json

---
+++ webapp/controller/OrdersPanel.controller.js
@@ -0,0 +1,20 @@
+sap.ui.define([
+    "sap/ui/core/mvc/Controller",
+    "sap/m/MessageToast",
+    "sap/ui/model/json/JSONModel",
+    "sap/ui/model/resource/ResourceModel"
+],
+    function (Controller, MessageToast) {
+        "use strict";
+
+        return Controller.extend("com.learn.ui5.sapui5basics.controller.OrdersPanel", {
+            onInit: function () {
+                // Set variables required on init of the view
+                
+            },
+            onPress: function () {
+                
+            },
+        
+        });
+    });
