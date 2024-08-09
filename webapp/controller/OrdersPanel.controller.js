cb178fd64197fff750406a107efa3b8c2c9e06cb
Avinash Vaidya <avinash.vaidya@sap.com>
2024-08-09 14:35:42 +0000

    Added data types and custom styles for currency

--- webapp/controller/OrdersPanel.controller.js
+++ webapp/controller/OrdersPanel.controller.js
@@ -1,20 +1,21 @@
 sap.ui.define([
     "sap/ui/core/mvc/Controller",
-    "sap/m/MessageToast",
     "sap/ui/model/json/JSONModel",
     "sap/ui/model/resource/ResourceModel"
 ],
-    function (Controller, MessageToast) {
+    function (Controller, JSONModel) {
         "use strict";
 
         return Controller.extend("com.learn.ui5.sapui5basics.controller.OrdersPanel", {
             onInit: function () {
                 // Set variables required on init of the view
+                var oViewModel = new JSONModel({
+                    currency: "EUR"
+                });
+
+                this.getView().setModel(oViewModel, "view")
                 
-            },
-            onPress: function () {
-                
-            },
+            }
         
         });
     });
