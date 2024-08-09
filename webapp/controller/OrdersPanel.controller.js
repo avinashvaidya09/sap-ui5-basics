3519a983686a1658f43f9503682c255b2e6f6816
Avinash Vaidya <avinash.vaidya@sap.com>
2024-08-09 19:53:06 +0000

    Added filtering at header to search for orders

--- webapp/controller/OrdersPanel.controller.js
+++ webapp/controller/OrdersPanel.controller.js
@@ -1,9 +1,11 @@
 sap.ui.define([
     "sap/ui/core/mvc/Controller",
     "sap/ui/model/json/JSONModel",
-    "../model/formatter"
+    "../model/formatter",
+    "sap/ui/model/Filter",
+    "sap/ui/model/FilterOperator"
 ],
-    function (Controller, JSONModel, formatter) {
+    function (Controller, JSONModel, formatter, Filter, FilterOperator) {
         "use strict";
 
         return Controller.extend("com.learn.ui5.sapui5basics.controller.OrdersPanel", {
@@ -16,6 +18,19 @@ sap.ui.define([
 
                 this.getView().setModel(oViewModel, "view")
                 
+            },
+            onFilterSalesOrders: function (oEvent) {
+                var aFilter = [];
+
+                var sQuery = oEvent.getParameter("query");
+
+                if (sQuery) {
+                    aFilter.push(new Filter("OrderNumber", FilterOperator.Contains, sQuery));
+                }
+
+                const oList = this.byId("ordersList");
+                const oBinding = oList.getBinding("items");
+                oBinding.filter(aFilter);
             }
             
         
